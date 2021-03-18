eac7b21ef9d751ca5bc14535df262847592f96cb
Sanjeev Rana <2020a1r122@mietjammu.in>
2021-03-18 21:49:22 +0530

    area and perimeter

--- area_3.c
+++ area_3.c
@@ -1,13 +1,13 @@
   
-#include<stdio.h>
+#include<stdio.h>//header file
 int main(){
 	float raduisOfCircle, sideOfSquare, legthOfRectangle, breadthOfRectangle;
 	scanf("%f %f %f %f", &raduisOfCircle, &sideOfSquare, &legthOfRectangle, &breadthOfRectangle);
-	printf("Area of Circle: %f",((22*raduisOfCircle*raduisOfCircle)/7));
-	printf("\nPerimeter of Circle: %f",((2*22*raduisOfCircle)/7));
-	printf("\nArea of Square: %f",(sideOfSquare*sideOfSquare));
-	printf("\nPerimeter of Square: %f",(4*sideOfSquare));
-	printf("\nArea of Rectangle: %f",(legthOfRectangle*breadthOfRectangle));
-	printf("\nPerimeter of Rectangle: %f",(2*(legthOfRectangle+breadthOfRectangle)));
+	printf("Area of Circle: %f",((22*raduisOfCircle*raduisOfCircle)/7));//formula for area of circle
+	printf("\nPerimeter of Circle: %f",((2*22*raduisOfCircle)/7));//formula for perimeter of circle
+	printf("\nArea of Square: %f",(sideOfSquare*sideOfSquare));//formula for area of square
+	printf("\nPerimeter of Square: %f",(4*sideOfSquare));//formula for perimeter of square 
+	printf("\nArea of Rectangle: %f",(legthOfRectangle*breadthOfRectangle));//formula for area of rectangle 
+	printf("\nPerimeter of Rectangle: %f",(2*(legthOfRectangle+breadthOfRectangle)));//formula for perimeter of rectangle
 	return 0;
 }
