82a7cfbd43739cf6cb41265c4374be653c24f9c6
Rajveer Rajput <2020a1r122@mietjammu.in>
2021-04-03 23:20:07 +0530

    elements in two dimensional array

--- 19print_array.c
+++ 19print_array.c
@@ -1,23 +1,30 @@
-//Program to read and print elements in 2-D array.
-#include<stdio.h>//header file
-int main()//main function
-{
-   /* 2D array declaration*/
+//To read and print elements in two dimensional array
+
+#include<stdio.h> //Pre-Process directive to include standard input output functions header file
+int main(){ // main function body starting
+   //2D array declaration
    int disp[2][3];
-   /Counter variables for the loop/
+
+   //Counter variables declaration for the loop
    int i, j;
-   for(i=0; i<2; i++) {
+
+   for(i=0; i<2; i++) { //Nested for loop
       for(j=0;j<3;j++) {
-         printf("Enter value for disp[%d][%d]:", i, j);
-         scanf("%d", &disp[i][j]);
+         printf("Enter value for disp[%d][%d]:", i, j); 
+         scanf("%d", &disp[i][j]); // taking input from the user
+
       }
    }
    //Displaying array elements
    printf("Two Dimensional array elements:\n");
-   for(i=0; i<2; i++) {
+   for(i=0; i<2; i++) { // Nested for loop
       for(j=0;j<3;j++) {
          printf("%d ", disp[i][j]);
          if(j==2){
             printf("\n");
+         }
+      }
+   }
    return 0;
-}
+   
+} //main function body ends
